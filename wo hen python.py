399cdd1a677492c419efd8740f8d9ff82e713327
Wcy2001 <8226248+wcy2001@user.noreply.gitee.com>
2020-12-08 21:09:34 +0800

    demo_new.py

---
+++ wo hen python.py
@@ -0,0 +1,11 @@
+def pythonsum(n) :
+    a = list(range(n))
+    b = list(range(n))
+    c = []
+
+    for i in range(len(a)):
+        a[i] = i ** 2
+        b[i] = i ** 3
+        c.append(a[i] + b[i])
+
+        return c 
